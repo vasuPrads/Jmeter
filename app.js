4de3ca8906921c420a4799be8a6109231a3b4b93
Vasu_Prads <vasudhevan090701@gmail.com>
2023-03-16 21:38:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import axios from 'axios';
 
 function JMeterButton() {
   const handleJMeterButtonClick = async () => {
-    try {
-      // Send a GET request to the start-jmeter route
-      const response = await axios.get('/start-jmeter');
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
-    }
+    // Set the path to the JMETER and test plan files
+    const jmeterPath = 'C:\\path\\to\\jmeter\\bin\\jmeter.bat';
+    const testPlanPath = 'C:\\path\\to\\test-plan.jmx';
+
+    // Launch the JMETER with the test plan
+    const { spawn } = window.require('child_process');
+    spawn('cmd.exe', ['/c', jmeterPath, '-n', '-t', testPlanPath], { detached: true, stdio: 'ignore' });
   };
 
   return (
