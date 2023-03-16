052f0622c579ce97acec1e2b566389cd09bca8d3
Vasu_Prads <vasudhevan090701@gmail.com>
2023-03-16 23:32:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,17 @@
-const express = require('express');
-const { exec } = require('child_process');
-const jmeter = require('jmeter');
-
-const app = express();
-const port = 3000;
-
-app.use(express.static('public'));
-
-app.get('/run-jmeter', (req, res) => {
-  const testPath = 'path/to/your/test.jmx';
-  const resultsPath = 'path/to/save/results.jtl';
-  const test = jmeter.testFromFile(testPath);
-  const options = { resultsFile: resultsPath };
-  test.run(options, (err, result) => {
-    if (err) {
-      console.error(`exec error: ${err}`);
-      return res.send(`exec error: ${err}`);
-    }
-    console.log(`stdout: ${result}`);
-    res.send(`<pre>${result}</pre>`);
-  });
-});
-
-app.listen(port, () => {
-  console.log(`Listening on http://localhost:${port}`);
-});
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Run JMeter</title>
+  </head>
+  <body>
+    <button onclick="runJMeter()">Run JMeter</button>
+    <div id="result"></div>
+    <script>
+      function runJMeter() {
+        fetch('/run-jmeter')
+          .then(response => response.text())
+          .then(html => document.getElementById('result').innerHTML = html);
+      }
+    </script>
+  </body>
+</html>
