1568cfc0e1cf95a164c2f6734a0a645452dac425
Vasu_Prads <vasudhevan090701@gmail.com>
2023-03-17 00:05:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Run JMeter</title>
-  </head>
-  <body>
-    <button onclick="runJMeter()">Run JMeter</button>
-    <div id="result"></div>
-    <script>
-      function runJMeter() {
-        fetch('/run-jmeter')
-          .then(response => response.text())
-          .then(html => document.getElementById('result').innerHTML = html);
-      }
-    </script>
-  </body>
-</html>
+const express = require('express');
+const { spawn } = require('child_process');
+
+const app = express();
+const jmeterPath = `${process.env.JMETER_HOME}/bin/jmeter.bat`;
+
+app.get('/run-jmeter', (req, res) => {
+  const jmeterTestFile = 'path/to/your/test/file.jmx';
+  const jmeterArgs = [
+    '-n',
+    '-t',
+    jmeterTestFile,
+    '-l',
+    'results.jtl',
+    '-e',
+    '-o',
+    'results'
+  ];
+
+  const jmeterProcess = spawn(jmeterPath, jmeterArgs);
+  
+  jmeterProcess.stdout.on('data', (data) => {
+    console.log(data.toString());
+  });
+  
+  jmeterProcess.stderr.on('data', (data) => {
+    console.error(data.toString());
+  });
+  
+  jmeterProcess.on('close', (code) => {
+    console.log(`JMETER process exited with code ${code}`);
+    res.sendFile(`${__dirname}/results/index.html`);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
