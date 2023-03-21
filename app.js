6da8e4869bce5bd8fec26b971a5a73f26f8d51b9
Vasu_Prads <vasudhevan090701@gmail.com>
2023-03-21 18:26:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,40 @@
-const express = require('express');
-const cors = require('cors');
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+import React, { useState } from 'react';
+import axios from 'axios';
 
-const app = express();
+const Form = () => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
 
-app.use(cors());
-app.use(express.json());
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = {
+      title: title,
+      description: description,
+    };
+    try {
+      await axios.post('http://localhost:5000/data', data);
+      setTitle('');
+      setDescription('');
+      alert('Data inserted successfully.');
+    } catch (error) {
+      console.log(error);
+      alert('Something went wrong.');
+    }
+  };
 
-const uri = 'mongodb://localhost:27017/your-db-name';
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Title:</label>
+        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+      </div>
+      <div>
+        <label>Description:</label>
+        <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
 
-app.post('/data', async (req, res) => {
-  try {
-    const client = await MongoClient.connect(uri);
-    const db = client.db();
-    const data = req.body;
-    await db.collection('your-collection-name').insertOne(data);
-    res.status(201).json({ message: 'Data inserted successfully.' });
-    client.close();
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: 'Something went wrong.' });
-  }
-});
-
-app.listen(5000, () => console.log('Server running on port 5000'));
+export default Form;
